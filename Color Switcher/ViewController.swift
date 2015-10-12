2e83a8f644594a509cdfd86f89de0acc08700894
Andrew Budziszek <abudziszek@gmail.com>
2015-10-12 12:43:40 -0500

    Initial Commit

--- Color Switcher/ViewController.swift
+++ Color Switcher/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myButton: UIButton!
+    
+    @IBAction func colorButton(sender: AnyObject) {
+        if(myButton.backgroundColor == UIColor.blueColor()){
+            myButton.backgroundColor = UIColor.blackColor()
+            myButton.setTitle("I AM BLACK!!", forState: UIControlState.Normal)
+        }else if(myButton.backgroundColor == UIColor.orangeColor())
+        {
+            myButton.backgroundColor = UIColor.blueColor()
+            myButton.setTitle("I AM BLUE!", forState: UIControlState.Normal)
+        }
+        else{
+            myButton.backgroundColor = UIColor.orangeColor()
+            myButton.setTitle("I AM ORANGE!", forState: UIControlState.Normal)        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
